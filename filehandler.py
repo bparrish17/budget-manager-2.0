407e2222efea4b3b98dc4e174a5cf319bea70077
Brian Parrish <brian.parrish3739@gmail.com>
2020-09-06 13:24:00 -0400

    documentation is 4 kool kids

--- filehandler.py
+++ filehandler.py
@@ -1,8 +1,17 @@
-
 import os, sys, shutil
 from constants import MONTH_MAP
 
-def make_folder_at_dir(target_dir, name):
+
+def make_folder_at_dir(target_dir: str, name: str) -> str:
+    """
+    Creates directory inside target parent directory by name
+    Args:
+        target_dir (str): directory to add folder to
+        name (str): name of directory to add
+
+    Returns:
+        Path of newly created folder
+    """
     try:
         path = os.path.join(target_dir, name)
         os.mkdir(path)
@@ -12,115 +21,170 @@ def make_folder_at_dir(target_dir, name):
     finally:
         return path
 
-def get_user_root_dir():
-  root_dir = os.path.dirname(os.path.abspath(__file__))
-  current_user = os.getlogin()
-  idx_for_downloads = root_dir.index(current_user) + len(current_user)
-  return f'{root_dir[0:idx_for_downloads]}'
 
-def find_file_by_str(arr, str):
-  iterable_arr = iter(arr)
-  result = ''
-  try:
-    result = next(val for val in iterable_arr if str in val and 'csv' in val.lower())
-  except StopIteration:
-    print(f'ERROR: {str} does not exist in {arr[0:5]}...\n')
-    raise
-  finally:
-    return result
+def get_user_root_dir() -> str:
+    """
+    Gets root directory path for the current user
+    Returns (str):
+        Root directory path for that user (MacOS)
+    """
+    root_dir = os.path.dirname(os.path.abspath(__file__))
+    current_user = os.getlogin()
+    idx_for_downloads = root_dir.index(current_user) + len(current_user)
+    return f'{root_dir[0:idx_for_downloads]}'
 
-def get_statements_from_downloads_dir(downloads_dir):
-  amex_file_key = 'activity'
-  usaa_file_key = 'bk_download'
-  chase_file_key = 'Chase7825'
 
-  filename_list = []
+def find_file_by_str(arr: list, string: str) -> str:
+    """
+    Finds file in list of files by string name
+    Args:
+        arr (list): list of files to look through
+        string (str): file name to look for
 
-  for (dirpath, dirnames, filenames) in os.walk(downloads_dir):
-      filename_list.extend(filenames)
-      break
-
-  amex_csv = find_file_by_str(filename_list, amex_file_key)
-  usaa_csv = find_file_by_str(filename_list, usaa_file_key)
-  chase_csv = find_file_by_str(filename_list, chase_file_key)
-  
-  return dict({ 'amex': amex_csv, 'usaa': usaa_csv, 'chase': chase_csv })
-
-def rename_files_by_key(root_dir, file_dict):
-  new_file_paths = {}
-
-  for file_name in file_dict:
-    full_path = f'{root_dir}/{file_dict[file_name]}'
-    new_file_name = f'{file_name}.csv'
-    new_full_path = f'{root_dir}/{new_file_name}'
+    Returns (str):
+        Path of found file
+    """
+    iterable_arr = iter(arr)
+    result = ''
     try:
-      os.rename(full_path, new_full_path)
-    except (NotADirectoryError, OSError) as e:
-      print(f'ERROR: renaming file "{file_name}" => {e}\n')
-      pass
+        result = next(val for val in iterable_arr if string in val and 'csv' in val.lower())
+    except StopIteration:
+        print(f'ERROR: {string} does not exist in {arr[0:5]}...\n')
+        raise
     finally:
-      new_file_paths.update({ new_file_name: new_full_path })
-
-  return new_file_paths
-
-def get_highest_dated_dir(parent_dir, parse_full):
-  dir_list = os.listdir(parent_dir)
-  highest_val = 0
-  idx_of_highest_val = 0
-
-  for idx, dir in enumerate(dir_list, start=0):
-    try:
-      if (parse_full == True):
-        curr_val = int(dir)
-      else:
-        curr_val = int(dir[0])
-
-      if (curr_val > highest_val):
-        highest_val = curr_val
-        idx_of_highest_val = idx
-    except ValueError:
-      print(f'ERROR: ValueError at {idx} of {dir} in get_highest_dated_dir\n')
-      pass
-  
-  return dir_list[idx_of_highest_val]
+        return result
 
 
-def main():
-  ROOT_DIR = get_user_root_dir()
-  downloads_dir = f'{ROOT_DIR}/Downloads'
-  budgeting_dir = f'{ROOT_DIR}/Documents/Home/Budgeting'
-  statement_files_dict = get_statements_from_downloads_dir(downloads_dir)
+def get_statements_from_downloads_dir(downloads_dir: str) -> dict:
+    """
+    Find the download financial statement CSVs in the expected directory
+    Args:
+        downloads_dir (str): OS downloads directory to look for statements
 
-  # change file names
-  renamed_statement_files_dict = rename_files_by_key(downloads_dir, statement_files_dict)
+    Returns (dict):
+        Dictionary of renamed financial statements keyed by their file name
+    """
+    amex_file_key = 'activity'
+    usaa_file_key = 'bk_download'
+    chase_file_key = 'Chase7825'
 
-  # get highest year directory from /Home/Budgeting => e.g. 2020
-  highest_year = get_highest_dated_dir(budgeting_dir, True)
-  highest_year_dir = f'{budgeting_dir}/{highest_year}'
+    filename_list = []
 
-  # get highest month directory from /Home/Budgeting/{highestYearDir}
-  highest_month = get_highest_dated_dir(highest_year_dir, False)
+    for (dirpath, dirnames, filenames) in os.walk(downloads_dir):
+        filename_list.extend(filenames)
+        break
 
-  # get new month directory to write
-  new_month_num = int(highest_month[0]) + 1
-  new_month = MONTH_MAP[new_month_num]
+    amex_csv = find_file_by_str(filename_list, amex_file_key)
+    usaa_csv = find_file_by_str(filename_list, usaa_file_key)
+    chase_csv = find_file_by_str(filename_list, chase_file_key)
 
-  # add new directory
-  new_month_dir = make_folder_at_dir(highest_year_dir, f'{new_month_num} - {new_month}')
+    return dict({'amex': amex_csv, 'usaa': usaa_csv, 'chase': chase_csv})
 
-  # add files to newly created directory
-  for file_key in renamed_statement_files_dict:
-    file_path = renamed_statement_files_dict[file_key]
-    try:
-      shutil.move(file_path, f'{new_month_dir}/{file_key}')
-    except:
-      print(f'ERROR: Could not move {file_key} from {file_path} to "{new_month_dir}/{file_key}"\n')
-      pass
 
-  print('Successfully Moved Downloaded Statement Files to Budgeting Folder')
-  return new_month_dir
+def rename_files_by_key(parent_dir, file_dict):
+    """
+    For each provided file in dict, rename them in their current parent directory
+    Args:
+      parent_dir (str): root directory for user (MacOS) (e.g. Users/<name>)
+      file_dict (dict<{ amex: str, usaa: str, chase: str }): dict with each csv path
 
-  # 1. read from downloads by known file name
-  # 2. rename each file to proper type (amex, usaa, chase)
-  # 3. make directory in Budget folder by month
-  # 4. add 3 files to budget / month folder
+    Returns (dict):
+      Dict keyed by file name with value of the new path of that file
+    """
+    new_file_paths = {}
+
+    for file_name in file_dict:
+        full_path = f'{parent_dir}/{file_dict[file_name]}'
+        new_file_name = f'{file_name}.csv'
+        new_full_path = f'{parent_dir}/{new_file_name}'
+        try:
+            os.rename(full_path, new_full_path)
+        except (NotADirectoryError, OSError) as e:
+            print(f'ERROR: renaming file "{file_name}" => {e}\n')
+            pass
+        finally:
+            new_file_paths.update({new_file_name: new_full_path})
+
+    return new_file_paths
+
+
+def get_highest_dated_dir(parent_dir, parse_full) -> str:
+    """
+    Get the latest dated directory in existing Budgeting folder
+    Args:
+      parent_dir (str): directory to parse through
+      parse_full (bool): whether or not parse full directory list
+
+    Returns (str):
+      Highest dated directory in parent directory (e.g. '8 - August')
+    """
+    dir_list = os.listdir(parent_dir)
+    highest_val = 0
+    idx_of_highest_val = 0
+
+    for idx, directory in enumerate(dir_list, start=0):
+        try:
+            if parse_full:
+                curr_val = int(directory)
+            else:
+                curr_val = int(directory[0])
+
+            if curr_val > highest_val:
+                highest_val = curr_val
+                idx_of_highest_val = idx
+        except ValueError:
+            print(f'ERROR: ValueError at {idx} of {directory} in get_highest_dated_dir\n')
+            pass
+
+    return dir_list[idx_of_highest_val]
+
+
+def main() -> str:
+    """
+    File Handler for Downloaded CSVs
+    1. Find downloaded financial statements by each file key
+    2. Renames files in place
+    3. Find latest month directory, then create one for next month (e.g. 8 - August)
+    4. Move renamed financial statement files to newly created month directory
+
+    Returns (str):
+      String path of new month directory (e.g. /Users/<name>/Documents/Home/Budgeting/8 - August)
+    """
+    root_dir = get_user_root_dir()
+    downloads_dir = f'{root_dir}/Downloads'
+    budgeting_dir = f'{root_dir}/Documents/Home/Budgeting'
+    statement_files_dict = get_statements_from_downloads_dir(downloads_dir)
+
+    # change file names
+    renamed_statement_files_dict = rename_files_by_key(downloads_dir, statement_files_dict)
+
+    # get highest year directory from /Home/Budgeting => e.g. 2020
+    highest_year = get_highest_dated_dir(budgeting_dir, True)
+    highest_year_dir = f'{budgeting_dir}/{highest_year}'
+
+    # get highest month directory from /Home/Budgeting/{highestYearDir}
+    highest_month = get_highest_dated_dir(highest_year_dir, False)
+
+    # get new month directory to write
+    new_month_num = int(highest_month[0]) + 1
+    new_month = MONTH_MAP[new_month_num]
+
+    # add new directory
+    new_month_dir = make_folder_at_dir(highest_year_dir, f'{new_month_num} - {new_month}')
+
+    # add files to newly created directory
+    for file_key in renamed_statement_files_dict:
+        file_path = renamed_statement_files_dict[file_key]
+        try:
+            shutil.move(file_path, f'{new_month_dir}/{file_key}')
+        except:
+            print(f'ERROR: Could not move {file_key} from {file_path} to "{new_month_dir}/{file_key}"\n')
+            pass
+
+    print('Successfully Moved Downloaded Statement Files to Budgeting Folder')
+    return new_month_dir
+
+    # 1. read from downloads by known file name
+    # 2. rename each file to proper type (amex, usaa, chase)
+    # 3. make directory in Budget folder by month
+    # 4. add 3 files to budget / month folder
--- sheetswriter.py
+++ sheetswriter.py
@@ -1,67 +1,94 @@
 import ezsheets
 
+
 # Sheet ('Expenses' | 'Incomes' | 'Expenses')
 # -- Column ('Date' | 'Amount' | 'Description' | 'Category')
 
-
-"""Updates Workbook Sheet With New Value
+def update_sheet(workbook, sheet_name: str, sheets_data: dict):
+    """
+    Update Workbook Sheet With Transactions
 
     Args:
-        Workbook: The Google Sheets Workbook
-        sheet_name (str): Name of the Sheet ('Totals', 'Expenses', 'Incomes', 'Investments')
+        workbook: Google Sheets Folder
+        sheet_name ('Expenses' | 'Incomes' | 'Expenses'): Sheet Tab in Google Sheets Folder
+        sheets_data (dict): dict of sheets_data keyd by sheet_name
 
     Returns:
-        A list of strings containing the words from the document.
+        void
 
-"""
-def update_sheet(Workbook, sheet_name: str, sheets_data: dict):
-  transaction_list = sheets_data[sheet_name.lower()]
-  transaction_list.sort(key=sort_by_date)
-  column_names = ['Date', 'Amount', 'Description', 'Category']
-  Sheet = Workbook[sheet_name]
-  date_column = Sheet.getColumn(1)
-  idx_to_add_data = get_last_value_in_data(date_column)
+    """
+    transaction_list = sheets_data[sheet_name.lower()]
+    transaction_list.sort(key=sort_by_date)
+    column_names = ['Date', 'Amount', 'Description', 'Category']
+    sheet = workbook[sheet_name]
+    date_column = sheet.getColumn(1)
+    idx_to_add_data = get_last_value_in_data(date_column)
 
-  # update column by column
-  for idx, column_name in enumerate(column_names, start=0):
-    col_idx = idx + 1 # column indices start at 1 in Google Sheets
-    current_column_values = Sheet.getColumn(col_idx)[0:idx_to_add_data]
-    updated_column = get_new_column_value(column_name, current_column_values, transaction_list)
-    Sheet.updateColumn(col_idx, updated_column)
+    # update column by column
+    for idx, column_name in enumerate(column_names, start=0):
+        col_idx = idx + 1  # column indices start at 1 in Google Sheets
+        current_column_values = sheet.getColumn(col_idx)[0:idx_to_add_data]
+        updated_column = get_new_column_value(column_name, current_column_values, transaction_list)
+        sheet.updateColumn(col_idx, updated_column)
 
 
 def get_new_column_value(
-  column_name: str,
-  current_column_values: list,
-  transactions_list: list
+        column_name: str,
+        current_column_values: list,
+        transactions_list: list
 ):
-  new_column = current_column_values.copy()
-  new_values = [transaction[column_name] for transaction in transactions_list]
-  new_column.extend(new_values)
-  
-  return new_column
+    """
+    Combines existing column values with new values from transactions
+    Args:
+        column_name ('Date' | 'Amount' | 'Description' | 'Category'): name of column to read
+        current_column_values (list<str>):
+        transactions_list (dict): transactions from CSV
+
+    Returns:
+        new_column: New combined list of column values
+    """
+    new_column = current_column_values.copy()
+    new_values = [transaction[column_name] for transaction in transactions_list]
+    new_column.extend(new_values)
+
+    return new_column
+
 
 def sort_by_date(transaction: dict) -> str:
-  return transaction['Date']
+    return transaction['Date']
+
 
 def get_last_value_in_data(column: list) -> int:
-  idx_of_last_value = 0
-  for idx, cell in enumerate(column, start=1):
-    if len(cell) == 0:
-      idx_of_last_value = idx
-      break
+    """
+    Gets
+    Args:
+        column (list):
 
-  return idx_of_last_value - 1
+    Returns (int):
+        index of last row that has a value in that column
+    """
+    idx_of_last_value = 0
+    for idx, cell in enumerate(column, start=1):
+        if len(cell) == 0:
+            idx_of_last_value = idx
+            break
+
+    return idx_of_last_value - 1
 
 
 def main(sheets_data: dict):
-  # test_spreadsheet_id = '1iRHLWOk7E_SPFO_n6Ok0xci__SUtApQGhseuOzz0ThI'
-  budget_spreadsheet_id = '1Kwlz2FG-nuHyQM0ieNsUZUfKTpq06OM-wnWcc3LT4Ko'
+    """
+    Iterates over each sheet and appends transactions
+    Args:
+        sheets_data (dict): existing transactions to add to sheets
+    """
+    # test_spreadsheet_id = '1iRHLWOk7E_SPFO_n6Ok0xci__SUtApQGhseuOzz0ThI'
+    budget_spreadsheet_id = '1Kwlz2FG-nuHyQM0ieNsUZUfKTpq06OM-wnWcc3LT4Ko'
 
-  Workbook = ezsheets.Spreadsheet(budget_spreadsheet_id)
-  for sheet_name in ['Expenses', 'Incomes', 'Investments']:
-    print(f'Writing {sheet_name} Transactions to Google Sheets...')
-    print('-------------------------------------------------------------------------')
-    update_sheet(Workbook, sheet_name, sheets_data)
+    workbook = ezsheets.Spreadsheet(budget_spreadsheet_id)
+    for sheet_name in ['Expenses', 'Incomes', 'Investments']:
+        print(f'Writing {sheet_name} Transactions to Google Sheets...')
+        print('-------------------------------------------------------------------------')
+        update_sheet(workbook, sheet_name, sheets_data)
 
-  print('\nSuccessfully Written Transactions to Google Sheets\n')
+    print('\nSuccessfully Written Transactions to Google Sheets\n')
